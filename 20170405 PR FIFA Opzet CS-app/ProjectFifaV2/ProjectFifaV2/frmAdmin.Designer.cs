7fc0df9eedca66af85ec430960e02ec48ea4ff42
Jarnotouw16 <d228459@edu.rocwb.nl>
2017-05-16 14:32:28 +0200

    add c#
    added all c# things i did

---
+++ 20170405 PR FIFA Opzet CS-app/ProjectFifaV2/ProjectFifaV2/frmAdmin.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ProjectFifaV2
+{
+    partial class frmAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.dgvAdminData = new System.Windows.Forms.DataGridView();
+            this.btnAdminLogOut = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.boxTableType = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdminData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(1005, 38);
+            this.btnLoadData.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(108, 25);
+            this.btnLoadData.TabIndex = 0;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(189, 41);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPath.MaxLength = 256;
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(685, 22);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(73, 41);
+            this.btnSelectFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(108, 25);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(73, 171);
+            this.txtQuery.Margin = new System.Windows.Forms.Padding(4);
+            this.txtQuery.MaxLength = 150;
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(685, 102);
+            this.txtQuery.TabIndex = 4;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(768, 207);
+            this.btnExecute.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(108, 25);
+            this.btnExecute.TabIndex = 5;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // dgvAdminData
+            // 
+            this.dgvAdminData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAdminData.Location = new System.Drawing.Point(16, 282);
+            this.dgvAdminData.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvAdminData.Name = "dgvAdminData";
+            this.dgvAdminData.Size = new System.Drawing.Size(1247, 334);
+            this.dgvAdminData.TabIndex = 6;
+            // 
+            // btnAdminLogOut
+            // 
+            this.btnAdminLogOut.Location = new System.Drawing.Point(1121, 41);
+            this.btnAdminLogOut.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdminLogOut.Name = "btnAdminLogOut";
+            this.btnAdminLogOut.Size = new System.Drawing.Size(141, 49);
+            this.btnAdminLogOut.TabIndex = 7;
+            this.btnAdminLogOut.Text = "Log Out";
+            this.btnAdminLogOut.UseVisualStyleBackColor = true;
+            this.btnAdminLogOut.Click += new System.EventHandler(this.btnAdminLogOut_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(873, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "choose wich table";
+            // 
+            // boxTableType
+            // 
+            this.boxTableType.FormattingEnabled = true;
+            this.boxTableType.Items.AddRange(new object[] {
+            "matches",
+            "players",
+            "teams"});
+            this.boxTableType.Location = new System.Drawing.Point(882, 41);
+            this.boxTableType.Name = "boxTableType";
+            this.boxTableType.Size = new System.Drawing.Size(121, 24);
+            this.boxTableType.TabIndex = 9;
+            // 
+            // frmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 630);
+            this.Controls.Add(this.boxTableType);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdminLogOut);
+            this.Controls.Add(this.dgvAdminData);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnLoadData);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmAdmin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdminData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.DataGridView dgvAdminData;
+        private System.Windows.Forms.Button btnAdminLogOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox boxTableType;
+    }
+}
---
+++ 20170405 PR FIFA Opzet CS-app/ProjectFifaV2/ProjectFifaV2/frmAdmin.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ProjectFifaV2
+{
+    public partial class frmAdmin : Form
+    {
+        private DatabaseHandler dbh;
+        private OpenFileDialog opfd;
+
+        DataTable table;
+
+        public frmAdmin()
+        {
+            dbh = new DatabaseHandler();
+            table = new DataTable();
+            this.ControlBox = false;
+            InitializeComponent();
+        }
+
+        private void btnAdminLogOut_Click(object sender, EventArgs e)
+        {
+            txtQuery.Text = null;
+            txtPath = null;
+            dgvAdminData.DataSource = null;
+            Hide();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (txtQuery.TextLength > 0)
+            {
+                ExecuteSQL(txtQuery.Text);
+            }
+        }
+
+        private void ExecuteSQL(string selectCommandText)
+        {
+            dbh.TestConnection();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(selectCommandText, dbh.GetCon());
+            dataAdapter.Fill(table);
+            dgvAdminData.DataSource = table;
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            txtPath.Text = null;
+
+            string path = GetFilePath();
+
+            if (CheckExtension(path, "csv"))
+            {
+                txtPath.Text = path;
+            }
+            else
+            {
+                MessageHandler.ShowMessage("The wrong filetype is selected.");
+            }
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+
+
+            if (!(txtPath.Text == null))
+            {
+
+                dbh.OpenConnectionToDB();
+
+                StreamReader sr = new StreamReader(txtPath.Text);
+
+                if (boxTableType.Text == "")
+                {
+                    MessageHandler.ShowMessage("you have to select an table.");
+                }
+                else if (boxTableType.Text == "matches")
+                {
+                    string data = sr.ReadLine();
+
+                    while (data != null)
+                    {
+                        string[] value = data.Split(',');
+                        
+                        int homeTeam = Convert.ToInt32(value[1]);
+                        int awayTeam = Convert.ToInt32(value[2]);
+                        int scoreHome = Convert.ToInt32(value[3]);
+                        int scoreAway = Convert.ToInt32(value[4]);
+
+                        data = sr.ReadLine();
+
+                        using (SqlCommand cmd = new SqlCommand("INSERT INTO TblGames ( HomeTeam, AwayTeam, HomeTeamScore, AwayTeamScore) VALUES (@homeTeam, @awayTeam, @scoreHome, @scoreAway)"))
+                        {
+                            cmd.Parameters.AddWithValue("@homeTeam", homeTeam);
+                            cmd.Parameters.AddWithValue("@awayTeam", awayTeam);
+                            cmd.Parameters.AddWithValue("@scoreHome", scoreHome);
+                            cmd.Parameters.AddWithValue("@scoreAway", scoreAway);
+                            cmd.Connection = dbh.GetCon();
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+
+                else if (boxTableType.Text == "players")
+                {
+                    string data = sr.ReadLine();
+
+                    while (data != null)
+                    {
+                        string[] value = data.Split(',');
+                        int id = Convert.ToInt32(value[0]);
+                        string name = value[3];
+                        string newName = name.Trim('"');
+                        string surname = value[4];
+                        string newSurname = surname.Trim('"');
+                        int goalScored = Convert.ToInt32(value[5]);
+                        int team_id = Convert.ToInt32(value[2]);
+                        data = sr.ReadLine();
+
+                        
+
+                        using (SqlCommand cmd = new SqlCommand("INSERT INTO TblPlayers (Name, Surname, GoalsScored, Team_id) VALUES (@name, @surname, @goalScored, @Team_id)"))
+                        {
+                                cmd.Parameters.AddWithValue("@name", newName);
+                                cmd.Parameters.AddWithValue("@surname", newSurname);
+                                cmd.Parameters.AddWithValue("@goalScored", goalScored);
+                                cmd.Parameters.AddWithValue("@Team_id", team_id);
+                                cmd.Connection = dbh.GetCon();
+                                cmd.ExecuteNonQuery();
+                            
+                        }
+
+                    }
+                }
+
+                else if (boxTableType.Text == "teams")
+                {
+                    string data = sr.ReadLine();
+
+                    while (data != null)
+                    {
+                        string[] value = data.Split(',');
+                        string TeamName = value[2];
+                        data = sr.ReadLine();
+                        
+                        using (SqlCommand cmd = new SqlCommand("INSERT INTO TblTeams (TeamName) VALUES (@teamName)"))
+                        {
+                                
+                                cmd.Parameters.AddWithValue("@teamName", TeamName);
+                                cmd.Connection = dbh.GetCon();
+                                cmd.ExecuteNonQuery();
+                            
+                        }
+                    }
+                }
+                MessageHandler.ShowMessage("data insert has been succeeded");
+                dbh.CloseConnectionToDB();
+            }
+            else
+            {
+                MessageHandler.ShowMessage("No filename selected.");
+            }
+        }
+
+
+        private string GetFilePath()
+        {
+            string filePath = "";
+            opfd = new OpenFileDialog();
+
+            opfd.Multiselect = false;
+
+            if (opfd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = opfd.FileName;
+            }
+
+            return filePath;
+        }
+
+        private bool CheckExtension(string fileString, string extension)
+        {
+            int extensionLength = extension.Length;
+            int strLength = fileString.Length;
+
+            string ext = fileString.Substring(strLength - extensionLength, extensionLength);
+
+            if (ext == extension)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
