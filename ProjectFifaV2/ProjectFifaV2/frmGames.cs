13fd8026f534a1a6aa0fd62a059ff5c1fe585287
dominic baeten <d228528@edu.rocwb.nl>
2017-06-06 14:23:38 +0200

    form games added

---
+++ ProjectFifaV2/ProjectFifaV2/frmGames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectFifaV2
+{
+    public partial class frmGames : Form
+    {
+        public frmGames()
+        {
+            InitializeComponent();
+        }
+    }
+}
